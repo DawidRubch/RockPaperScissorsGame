62068cc3c4291d3be10848f2e74bb295b0351e52
DawidRubch <dawid.niegrebecki@gmail.com>
2020-08-29 13:10:51 +0200

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Layout from "./pages/mainLayout";
+
+function App() {
+  return (
+    <div>
+      <Layout></Layout>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/mainLayout.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import styles from "./mainLayout.module.css";
+const Layout: React.FC = () => {
+  return (
+    <div>
+      <span className={styles.circle}></span>
+    </div>
+  );
+};
+
+export default Layout;
